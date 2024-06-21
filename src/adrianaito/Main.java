3f2faca451f3bd6583e428aac764f858ebe1ed14
Adriana Ito <ito.dri@gmail.com>
2024-06-21 15:49:21 +0900

    Array methods exercise

---
+++ src/adrianaito/Main.java
@@ -0,0 +1,10 @@
+package adrianaito;
+
+public class Main {
+    public static void main(String[] names) {
+        Array numbers = new Array(3);
+        numbers.print();
+        numbers.insert(10);
+        numbers.print();
+    }
+}
